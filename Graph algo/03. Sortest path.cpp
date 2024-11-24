20ed1e1fb5a24a78a65c8990e5d37232fb726d8f
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-25 03:53:32 +0600

    1 theke jekono node er shortest path

--- Graph algo/03. Sortest path.cpp
+++ Graph algo/03. Sortest path.cpp
@@ -1,9 +1,5 @@
-/* 
-Find shortest path using bfs and storing parent which from come?
-
 problem:
 Find sortest path 1 to 8;
-
 Input: 
 8 8
 1 2 
@@ -16,52 +12,61 @@ Input:
 7 8
 output:
 8 7 4 1
+->> visualization of graph:    [https://raw.githubusercontent.com/Rabbi-hasan0/Course-phase-01/refs/heads/main/Graph%20algo/Graph-visual/1.png]
 
-*/
 
-// Code:
-<img src ="https://raw.githubusercontent.com/Rabbi-hasan0/Course-phase-01/refs/heads/main/Graph%20algo/Graph-visual/1.png"> 
+
+// Code: 
 
 #include<bits/stdc++.h>
 using namespace std;
+
 const int N = 1e5 + 7;
-
-vector<int> graph[N];
+vector<int> g[N];
 bool vis[N];
-int dis[N], par[N];
+int dis[N], parent[N];
+queue<int> q;
 
-int32_t main(){
-    int node, edges;
-    cin >> node >> edges;
-    while(edges--) {
-        int u, v;cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    queue<int> q;
-    q.push(1);vis[1] = true;dis[1] = 0;
+void bfs(int st) {
+    q.push(st);
+    vis[st] = true;
+    dis[1] = 0;           // define layer 0; 
     while(!q.empty()) {
         int u = q.front();
         q.pop();
-        for(auto i: graph[u]) {
+        for(auto i: g[u]) {
             if(!vis[i]) {
                 q.push(i);
-                par[i] = u;
-                dis[i] = dis[u] + 1;
+                parent[i] = u;    // এই নোডের parent কোনটা ওইটা সেভ করে রাখছি, লুপের নোডগুলার parent তোহ u তাই সব নোডে u save করে রাখলাম।
+                dis[i] = dis[u] + 1;      // new layer e jacci eikhan theke so (ager layer er sathe 1 add kore dicci);
                 vis[i] = true;
             }
         }
     }
-    for(int i = 1; i <= node; i++) {
-        cout << dis[i] << ' ';
-    }cout << '\n';
+}
 
-    int v = 8;   // 1 to v porjonto shortest path khuje ber korbe
+void print_path(int v) {
     while(v != 1) {
         cout << v << ' ';
-        v = par[v];
+        v = parent[v];
     }
     cout << 1 << '\n';
+}
+
+int32_t main(){
+    int n, e;
+    cin >> n >> e;
+    while(e--) {
+        int u, v;cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    bfs(1);
+    for(int i = 1; i <= n; i++) { // Kon layer e koto gula node ache eta dekha jai
+        cout << dis[i] << ' ';
+    }cout << '\n';
+    cout << dis[8] << '\n'; // Print 8 koto number layer e ache..!
+    print_path(6); // 6 theke 1 e jabar path print koro, jehetu bfs chalacci tai eitai shortest path
 
     return 0;
 }
