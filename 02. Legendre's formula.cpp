49073baaf07143f2162c5cb763da42db51b1d797
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-27 01:31:37 +0600

    Update 02. Legendre's formula.cpp

--- 02. Legendre's formula.cpp
+++ 02. Legendre's formula.cpp
@@ -1,20 +1,22 @@
+## এখানে (n!) এর মধ্যে সর্বোচ্চ p এর কত পাওয়ার দিয়ে ভাগ যাবে ওইটা বের করতেছে।  Complexity: O(logn).. 
 
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int legendre(long long n, long long p) {
+int legendre(int n, int p) {              
   int ans = 0;
   while (n) {
-    ans += n / p;      // This work = (n/p^1 + n/(p^2) + ... + n/(p^n)
+    ans += n / p;              // This work = (n/p^1 + n/(p^2) + ... + n/(p^n)
     n /= p;
   }
   return ans;
 }
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n; cin >> n;
-  cout << "The largest power of" << p << "that divides " << " n! " << legendre(n, p) << '\n';
-  return 0;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, p; cin >> n >> p;
+    cout << "The largest power of " << p << " that divides " << n << "! is " << legendre(n, p) << '\n';
+    return 0;
 }
