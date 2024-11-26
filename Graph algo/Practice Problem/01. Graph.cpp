0da49a6c975627ac4e67312f7022d77ecc8148c1
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-27 02:11:41 +0600

    Japanies
    https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_A

--- Graph algo/Practice Problem/01. Graph.cpp
+++ Graph algo/Practice Problem/01. Graph.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 #define print(x) cout << x << '\n'
+#define int long long
 using namespace std;
-
-const int N = 110;
-int g[N][N];
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;cin >> t;
-    int n = t;
-    while(t--){
-        int u, k;cin >> u >> k;
-        while(k--) {
-          int v;cin >> v;
-          g[u][v] = 1;
+    int a[t + 1][t + 1];
+    for(int i = 1; i <= t; ++i) {
+        for(int j = 1; j <= t; ++j) {
+            a[i][j] = 0;
         }
     }
-    for(int i = 1; i <= n; i++) {
-      for(int j = 1; j <= n; j++) {
-        cout << g[i][j] << ' ';
-      }cout << '\n';
-    }cout << '\n';
+    for(int i = 1; i <= t; i++){
+        int u, k; cin >> u >> k;
+        while(k--) {
+            int x; cin >> x;
+            a[u][x] = 1;
+        }
+    }
+    for(int i = 1; i <= t; ++i) {
+        for(int j = 1; j <= t; ++j) {
+            cout << a[i][j] << ' ';
+        } cout << '\n';
+    } cout << '\n';
+    
     return 0;
 }
