5ca197f7de3810ba25208c257e73b4581ab6276f
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2024-11-27 03:49:52 +0600

    Depth first Search
    https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B

---
+++ Graph algo/Practice Problem/02. Depth first Search.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define print(x) cout << x << '\n'
+#define int long long
+using namespace std;
+
+const int N = 110;
+vector<int> g[N];
+bool vis[N];
+int start[N], finish[N], cur;
+void dfs(int u) {
+    vis[u] = true;
+    start[u] = ++cur;
+    for(auto v: g[u]) {
+        if(!vis[v]) dfs(v);
+    }
+    finish[u] = ++cur;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;cin >> t;
+    for(int i = 1; i <= t; i++){
+        int u, k; cin >> u >> k;
+        while(k--) {
+            int v; cin >> v;
+            g[u].push_back(v);
+        }
+        // cout << u << ':';
+        // for(auto v: g[u]) cout << v << ' '; cout << '\n';
+        sort(g[u].begin(), g[u].end());
+    }
+    cur = 0;
+    for(int i = 1; i <= t; ++i) {
+        if(!vis[i]) dfs(i);
+    }
+    for(int i = 1; i <= t; ++i) {
+        cout << i << ' ' << start[i] << ' ' << finish[i] << '\n';
+    }
+    
+    return 0;
+}
